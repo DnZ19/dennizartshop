ab4e99ad1919f7502b93b6747abc300435faa5b6
DnZ19 <dennis@centerofagile.nl>
2023-04-25 20:00:02 +0200

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav>
+          <Link href="/about">Go to about</Link>
+          <Link href="/">Go home</Link>
+        </nav>
+        {children}
+        </body>
     </html>
   )
 }
